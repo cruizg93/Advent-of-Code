ca66408b3dfb1df9df340ae3f078926c329b464b
cruizg93 <crisruiz93@gmail.com>
2016-11-01 19:19:26 -0400

    fixed

--- 2015/day2/Problem2.py
+++ 2015/day2/Problem2.py
@@ -1,26 +1,15 @@
 #DOES NOT WORK!!!!
 #http://adventofcode.com/2015/day/2
+#Grab the two smallest sides multiple each by 2 and added together
+#then multiple every side with each other and the total added to the other operation result
 import sys
-total = 0
+totalPaper = 0
 for line in sys.stdin.readlines():
     dimensions = line.replace('\n', '').split('x')
-    l = int(dimensions[0])
-    w = int(dimensions[1])
-    h = int(dimensions[2])
-    extra = 0
-    
-    side1 = l+l+w+w
-    side2 = l+l+h+h
-    side3 = w+w+h+h
-    area = l*w*h
-    
-    if side1 < side2 and side1 < side3:
-        extra = side1
-    elif side2< side3 and side2 < side1:
-        extra = side2
-    elif side3 < side1 and side3 < side2:
-        extra = side3
-    total += extra+area
-
-print(total)
+    dimensions = list(map(int,dimensions))
+    dimensions.sort()
+    wrap = (dimensions[0]*2)+(dimensions[1]*2)
+    ribbon = dimensions[0]*dimensions[1]*dimensions[2]
+    totalPaper+=wrap+ribbon
+print(totalPaper)
     
