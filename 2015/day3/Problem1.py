88d8d9b17822545fdb85b26d1adc94797a9e6cf9
cruizg93 <crisruiz93@gmail.com>
2016-11-01 14:41:11 -0400

    Add the initial cordinate to the houses list
    #version 2 - fixed bug. Add the initial cordinate to the houses list

--- 2015/day3/Problem1.py
+++ 2015/day3/Problem1.py
@@ -2,12 +2,14 @@
 #First Problem
 #Solution: think about initial position x=0 & y=0 in a cartesian plane, and register every step than santa goes.
 #if santa has been in that coordinate that mean he is been there already.
+
+#version 2 - fixed bug. Add the initial cordinate to the houses list
 puzzle = input()
 addresses = list(puzzle)
 house = 1
 x = 0
 y = 0
-houses = []
+houses = ['0,0']
 for currentAddress in addresses:
     if currentAddress == '>':
        x = x+1
@@ -18,10 +20,9 @@ for currentAddress in addresses:
     elif currentAddress == '^':
         y = y+1
     
-    position = "'{},{}'".format(x, y)
+    position = '{},{}'.format(x, y)
     if not position in houses:
         houses.append(position)
         house = house + 1
     
 print(house)
-    
