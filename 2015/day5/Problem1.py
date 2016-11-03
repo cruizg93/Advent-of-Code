6e38afcf1b04dad64bbc2b5b8385e64fbb9b2bb4
cruizg93 <crisruiz93@gmail.com>
2016-11-03 12:13:29 -0400

    updated problem1 OK

--- 2015/day5/Problem1.py
+++ 2015/day5/Problem1.py
@@ -3,19 +3,36 @@ import sys
 vowels = ['a', 'e', 'i', 'o', 'u']
 nogoods = ['ab', 'cd', 'pq', 'xy']
 niceWordCont = 0
+howManyNice = 0
 def checkVowels(characterArray):
+    vowelCont = 0
     for character in characterArray:
         if character in vowels:
+            vowelCont += 1
+        if vowelCont == 3:
             return True
     return False
 
 def checkDoubleChar(characterArray):
-    indexCount =0
+    indexCount =1
     for character in characterArray:
-        if indexCount < characterArray.lenght():
+        if indexCount < len(characterArray):
+            if character == characterArray[indexCount]:
+                return True
+        indexCount += 1        
+    return False
             
-
+def haskNoGoods(line):
+    for bad in nogoods:
+        if bad in line:
+            return True
+    return False  
+    
 for line in sys.stdin.readlines():
     character = list(line)
-    if checkVowels(character) == True and  
-    print(checkVowels(character))
+    haskNoGoods(character)
+    if checkVowels(character) and checkDoubleChar(character):
+        if not haskNoGoods(line):
+            howManyNice += 1
+    
+print(howManyNice)
